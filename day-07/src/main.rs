3a2d4fbcdfdcb34a9999232699bfdb480765166b
Kenneth Collins <dev@kecors.net>
2022-02-20 13:50:40 -0500

    Solve day 7, part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,29 +1,31 @@
 use std::io::{stdin, Read};
+use std::result::Result;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
 
 #[derive(Debug)]
-enum Status {
-    Normal,
-    Output(i32),
-    Terminated,
-}
-
-#[derive(Debug)]
-struct Program {
+struct Amplifier {
     memory: Vec<i32>,
     ip: usize,
-    input: Vec<i32>,
+    rx: Receiver<i32>,
+    tx: Sender<i32>,
 }
 
-impl Program {
-    fn new(integers: &[i32], input: Vec<i32>) -> Program {
-        Program {
-            memory: integers.to_vec(),
+impl Amplifier {
+    fn new(program: &[i32], rx: Receiver<i32>, tx: Sender<i32>) -> Amplifier {
+        Amplifier {
+            memory: program.to_vec(),
             ip: 0,
-            input,
+            rx,
+            tx,
         }
     }
 
-    fn step(&mut self) -> Status {
+    fn run(&mut self) {
+        while self.step() {}
+    }
+
+    fn step(&mut self) -> bool {
         let opcode = self.memory[self.ip] % 100;
 
         match opcode {
@@ -50,7 +52,7 @@ impl Program {
 
                 self.memory[parameter_3 as usize] = value_1 + value_2;
 
-                Status::Normal
+                true
             }
             2 => {
                 // Multiply
@@ -75,16 +77,16 @@ impl Program {
 
                 self.memory[parameter_3 as usize] = value_1 * value_2;
 
-                Status::Normal
+                true
             }
             3 => {
                 // Input
                 let parameter_1 = self.memory[self.ip + 1];
                 self.ip += 2;
 
-                self.memory[parameter_1 as usize] = self.input.pop().unwrap();
+                self.memory[parameter_1 as usize] = self.rx.recv().unwrap();
 
-                Status::Normal
+                true
             }
             4 => {
                 // Output
@@ -99,7 +101,9 @@ impl Program {
                     _ => panic!("Unknown parameter mode {}", parameter_1_mode),
                 };
 
-                Status::Output(value_1)
+                let _ = self.tx.send(value_1);
+
+                true
             }
             5 => {
                 // Jump if true
@@ -125,7 +129,7 @@ impl Program {
                     self.ip = value_2 as usize;
                 }
 
-                Status::Normal
+                true
             }
             6 => {
                 // Jump if false
@@ -151,7 +155,7 @@ impl Program {
                     self.ip = value_2 as usize;
                 }
 
-                Status::Normal
+                true
             }
             7 => {
                 // Less than
@@ -176,7 +180,7 @@ impl Program {
 
                 self.memory[parameter_3 as usize] = if value_1 < value_2 { 1 } else { 0 };
 
-                Status::Normal
+                true
             }
             8 => {
                 // Equal
@@ -201,9 +205,9 @@ impl Program {
 
                 self.memory[parameter_3 as usize] = if value_1 == value_2 { 1 } else { 0 };
 
-                Status::Normal
+                true
             }
-            99 => Status::Terminated,
+            99 => false,
             _ => {
                 unimplemented!();
             }
@@ -211,28 +215,48 @@ impl Program {
     }
 }
 
-fn run(integers: &[i32], phase_settings: &[i32]) -> i32 {
-    let mut input = 0;
+fn run(program: &[i32], phase_settings: &[i32]) -> i32 {
+    let (tx_a, rx_a) = channel();
+    let (tx_b, rx_b) = channel();
+    let (tx_c, rx_c) = channel();
+    let (tx_d, rx_d) = channel();
+    let (tx_e, rx_e) = channel();
 
-    for &phase_setting in phase_settings.iter() {
-        let mut program = Program::new(integers, vec![input, phase_setting]);
-        loop {
-            match program.step() {
-                Status::Normal => (),
-                Status::Output(output) => input = output,
-                Status::Terminated => break,
-            }
+    let (tx_master, rx_master) = channel();
+
+    tx_a.send(phase_settings[0]).unwrap();
+    tx_b.send(phase_settings[1]).unwrap();
+    tx_c.send(phase_settings[2]).unwrap();
+    tx_d.send(phase_settings[3]).unwrap();
+    tx_e.send(phase_settings[4]).unwrap();
+
+    let mut amplifier_a = Amplifier::new(program, rx_a, tx_b);
+    let mut amplifier_b = Amplifier::new(program, rx_b, tx_c);
+    let mut amplifier_c = Amplifier::new(program, rx_c, tx_d);
+    let mut amplifier_d = Amplifier::new(program, rx_d, tx_e);
+    let mut amplifier_e = Amplifier::new(program, rx_e, tx_master);
+
+    thread::spawn(move || amplifier_a.run());
+    thread::spawn(move || amplifier_b.run());
+    thread::spawn(move || amplifier_c.run());
+    thread::spawn(move || amplifier_d.run());
+    thread::spawn(move || amplifier_e.run());
+
+    tx_a.send(0).unwrap();
+    loop {
+        let value = rx_master.recv().unwrap();
+        match tx_a.send(value) {
+            Result::Ok(_) => (),
+            Result::Err(_) => return value,
         }
     }
-
-    input
 }
 
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
 
-    let integers: Vec<i32> = input
+    let program: Vec<i32> = input
         .split(',')
         .map(|x| x.trim().parse::<i32>().unwrap())
         .collect();
@@ -257,7 +281,7 @@ fn main() {
                         if e == a || e == b || e == c || e == d {
                             continue;
                         }
-                        let output = run(&integers, &[a, b, c, d, e]);
+                        let output = run(&program, &[a, b, c, d, e]);
                         if output > maximum_output {
                             maximum_output = output;
                         }
@@ -268,4 +292,36 @@ fn main() {
     }
 
     println!("Part 1: the highest signal is {}", maximum_output);
+
+    // Part 2
+    let mut maximum_output = 0;
+
+    for a in 5..=9 {
+        for b in 5..=9 {
+            if b == a {
+                continue;
+            }
+            for c in 5..=9 {
+                if c == a || c == b {
+                    continue;
+                }
+                for d in 5..=9 {
+                    if d == a || d == b || d == c {
+                        continue;
+                    }
+                    for e in 5..=9 {
+                        if e == a || e == b || e == c || e == d {
+                            continue;
+                        }
+                        let output = run(&program, &[a, b, c, d, e]);
+                        if output > maximum_output {
+                            maximum_output = output;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Part 2: the highest signal is {}", maximum_output);
 }
