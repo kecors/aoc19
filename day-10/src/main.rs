464bb62e8fedcd535275ef16199674e999848dc3
Kenneth Collins <dev@kecors.net>
2022-02-27 13:39:35 -0500

    Solve day 10, part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,124 +1,279 @@
-use std::collections::HashSet;
 use std::fmt;
 use std::io::{stdin, Read};
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-struct Position {
+#[derive(Debug, PartialEq, Clone)]
+enum State {
+    Unscanned,
+    Detectable,
+    Undetectable,
+    Vaporized,
+}
+
+#[derive(Clone)]
+struct Neighbor {
     x: usize,
     y: usize,
+    delta_x: isize,
+    delta_y: isize,
+    state: State,
 }
 
-impl Position {
-    fn new(x: usize, y: usize) -> Position {
-        Position { x, y }
-    }
-
-    fn relation_to(&self, other: &Position) -> Relation {
-        let x_delta = other.x as isize - self.x as isize;
-        let y_delta = other.y as isize - self.y as isize;
-
-        Relation {
-            position: other.clone(),
-            x_delta,
-            y_delta,
-            visible: true,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-struct Relation {
-    position: Position,
-    x_delta: isize,
-    y_delta: isize,
-    visible: bool,
-}
-
-impl fmt::Display for Relation {
+impl fmt::Display for Neighbor {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
             "{}",
             &format!(
-                "({},{}): ({},{}) {:?}",
-                self.position.x, self.position.y, self.x_delta, self.y_delta, self.visible
+                "<{},{}> ({},{}) {:?}",
+                self.x, self.y, self.delta_x, self.delta_y, self.state
             )
         )
     }
 }
 
-impl Relation {
-    fn is_hidden_by(&self, other: &Relation) -> bool {
-        if (self.x_delta > 0) != (other.x_delta > 0) {
+impl Neighbor {
+    fn new(x: usize, y: usize, scanner_x: usize, scanner_y: usize) -> Neighbor {
+        let delta_x = x as isize - scanner_x as isize;
+        let delta_y = y as isize - scanner_y as isize;
+        let state = State::Unscanned;
+
+        Neighbor {
+            x,
+            y,
+            delta_x,
+            delta_y,
+            state,
+        }
+    }
+
+    fn is_hidden_by(&self, other: &Neighbor) -> bool {
+        if (self.delta_x > 0) != (other.delta_x > 0) {
             false
-        } else if (self.y_delta > 0) != (other.y_delta > 0) {
+        } else if (self.delta_y > 0) != (other.delta_y > 0) {
             false
-        } else if self.x_delta == 0 {
-            other.x_delta == 0 && self.y_delta.abs() > other.y_delta.abs()
-        } else if self.y_delta == 0 {
-            other.y_delta == 0 && self.x_delta.abs() > other.x_delta.abs()
-        } else if self.x_delta.abs() <= other.x_delta.abs() {
+        } else if self.delta_x == 0 {
+            other.delta_x == 0 && self.delta_y.abs() > other.delta_y.abs()
+        } else if self.delta_y == 0 {
+            other.delta_y == 0 && self.delta_x.abs() > other.delta_x.abs()
+        } else if self.delta_x.abs() <= other.delta_x.abs() {
             false
-        } else if self.y_delta.abs() <= other.y_delta.abs() {
+        } else if self.delta_y.abs() <= other.delta_y.abs() {
             false
         } else {
-            self.x_delta * other.y_delta == other.x_delta * self.y_delta
+            self.delta_x * other.delta_y == other.delta_x * self.delta_y
         }
     }
 }
 
 #[derive(Debug)]
+enum Direction {
+    Up,
+    Left,
+    Down,
+    Right,
+}
+
+#[derive(Debug)]
+enum Quadrant {
+    UpperRight,
+    LowerRight,
+    LowerLeft,
+    UpperLeft,
+}
+
+struct Scanner {
+    x: usize,
+    y: usize,
+    neighbors: Vec<Neighbor>,
+    vaporizations: Vec<Neighbor>,
+}
+
+impl fmt::Display for Scanner {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut result = String::new();
+
+        result.push_str(&format!("scanner at ({},{}):\n", self.x, self.y));
+
+        for neighbor in self.neighbors.iter() {
+            result.push_str(&format!("\t{}\n", neighbor));
+        }
+
+        if !self.vaporizations.is_empty() {
+            result.push_str("Vaporizations:\n");
+        }
+        for (index, vaporization) in self.vaporizations.iter().enumerate() {
+            result.push_str(&format!("\t{}: {}\n", index, vaporization));
+        }
+
+        write!(f, "{}", result)
+    }
+}
+
+impl Scanner {
+    fn new(x: usize, y: usize, positions: &[(usize, usize)]) -> Scanner {
+        let mut neighbors = Vec::new();
+
+        for position in positions.iter() {
+            if x == position.0 && y == position.1 {
+                continue;
+            }
+            neighbors.push(Neighbor::new(position.0, position.1, x, y));
+        }
+
+        let vaporizations = Vec::new();
+
+        Scanner {
+            x,
+            y,
+            neighbors,
+            vaporizations,
+        }
+    }
+
+    fn scan(&mut self) {
+        for j in 0..self.neighbors.len() {
+            if self.neighbors[j].state == State::Vaporized {
+                continue;
+            }
+            self.neighbors[j].state = State::Detectable;
+
+            for k in 0..self.neighbors.len() {
+                if j == k {
+                    continue;
+                }
+                if self.neighbors[k].state == State::Vaporized {
+                    continue;
+                }
+                if self.neighbors[j].is_hidden_by(&self.neighbors[k]) {
+                    self.neighbors[j].state = State::Undetectable;
+                }
+            }
+        }
+    }
+
+    fn detectable_positions(&self) -> usize {
+        self.neighbors
+            .iter()
+            .filter(|neighbor| neighbor.state == State::Detectable)
+            .count()
+    }
+
+    fn vaporize_all_asteroids(&mut self) {
+        loop {
+            self.scan();
+
+            self.vaporize_direction(Direction::Up);
+            self.vaporize_quadrant(Quadrant::UpperRight, false);
+            self.vaporize_direction(Direction::Right);
+            self.vaporize_quadrant(Quadrant::LowerRight, true);
+            self.vaporize_direction(Direction::Down);
+            self.vaporize_quadrant(Quadrant::LowerLeft, false);
+            self.vaporize_direction(Direction::Left);
+            self.vaporize_quadrant(Quadrant::UpperLeft, true);
+
+            if self
+                .neighbors
+                .iter()
+                .all(|neighbor| neighbor.state == State::Vaporized)
+            {
+                break;
+            }
+        }
+    }
+
+    fn vaporize_direction(&mut self, direction: Direction) {
+        if let Some(neighbor) = self
+            .neighbors
+            .iter_mut()
+            .filter(|neighbor| neighbor.state == State::Detectable)
+            .find(|neighbor| match direction {
+                Direction::Up => neighbor.delta_x == 0 && neighbor.delta_y < 0,
+                Direction::Left => neighbor.delta_y == 0 && neighbor.delta_x > 0,
+                Direction::Down => neighbor.delta_x == 0 && neighbor.delta_y > 0,
+                Direction::Right => neighbor.delta_y == 0 && neighbor.delta_x < 0,
+            })
+        {
+            neighbor.state = State::Vaporized;
+            self.vaporizations.push(neighbor.clone());
+        }
+    }
+
+    fn vaporize_quadrant(&mut self, quadrant: Quadrant, flip: bool) {
+        let mut indexes = Vec::new();
+        for (index, _neighbor) in self
+            .neighbors
+            .iter()
+            .enumerate()
+            .filter(|(_index, neighbor)| neighbor.state == State::Detectable)
+            .filter(|(_index, neighbor)| match quadrant {
+                Quadrant::UpperRight => neighbor.delta_x > 0 && neighbor.delta_y < 0,
+                Quadrant::LowerRight => neighbor.delta_x > 0 && neighbor.delta_y > 0,
+                Quadrant::LowerLeft => neighbor.delta_x < 0 && neighbor.delta_y > 0,
+                Quadrant::UpperLeft => neighbor.delta_x < 0 && neighbor.delta_y < 0,
+            })
+        {
+            indexes.push(index);
+        }
+
+        indexes.sort_by(|&a, &b| {
+            let a_delta_x: f32 = self.neighbors[a].delta_x.abs() as f32;
+            let a_delta_y: f32 = self.neighbors[a].delta_y.abs() as f32;
+            let a_ratio = a_delta_x / a_delta_y;
+
+            let b_delta_x: f32 = self.neighbors[b].delta_x.abs() as f32;
+            let b_delta_y: f32 = self.neighbors[b].delta_y.abs() as f32;
+            let b_ratio = b_delta_x / b_delta_y;
+
+            if flip {
+                b_ratio.partial_cmp(&a_ratio).unwrap()
+            } else {
+                a_ratio.partial_cmp(&b_ratio).unwrap()
+            }
+        });
+
+        for &index in indexes.iter() {
+            self.neighbors[index].state = State::Vaporized;
+            self.vaporizations.push(self.neighbors[index].clone());
+        }
+    }
+}
+
 struct Region {
-    positions: Vec<Position>,
+    scanners: Vec<Scanner>,
+}
+
+impl fmt::Display for Region {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut result = String::new();
+
+        for scanner in self.scanners.iter() {
+            result.push_str(&format!("{}", scanner));
+        }
+
+        write!(f, "{}", result)
+    }
 }
 
 impl Region {
     fn new(input: &str) -> Region {
-        let mut positions: Vec<Position> = Vec::new();
+        let mut positions: Vec<(usize, usize)> = Vec::new();
 
         input.lines().enumerate().for_each(|(y, line)| {
             line.chars().enumerate().for_each(|(x, ch)| {
                 if ch != '.' {
-                    positions.push(Position::new(x, y));
+                    positions.push((x, y));
                 }
             });
         });
 
-        Region { positions }
-    }
-
-    fn calculate_maximum_detectable_relations(&mut self) -> usize {
-        let mut maximum_detectable_relations = 0;
-
-        for position_1 in self.positions.iter() {
-            let mut relations: Vec<Relation> = Vec::new();
-            for position_2 in self.positions.iter() {
-                if position_1 == position_2 {
-                    continue;
-                }
-                relations.push(position_1.relation_to(position_2));
-            }
-
-            let mut undetectable_relations = HashSet::new();
-            for (index, relation_1) in relations.iter().enumerate() {
-                for relation_2 in relations.iter() {
-                    if relation_1 == relation_2 {
-                        continue;
-                    }
-                    if relation_1.is_hidden_by(relation_2) {
-                        undetectable_relations.insert(index);
-                        break;
-                    }
-                }
-            }
-
-            let detectable_relations = relations.len() - undetectable_relations.len();
-            if detectable_relations > maximum_detectable_relations {
-                maximum_detectable_relations = detectable_relations;
-            }
+        let mut scanners = Vec::new();
+        for position in positions.iter() {
+            let mut scanner = Scanner::new(position.0, position.1, &positions);
+            scanner.scan();
+            scanners.push(scanner);
         }
 
-        maximum_detectable_relations
+        Region { scanners }
     }
 }
 
@@ -129,8 +284,21 @@ fn main() {
     // Part 1
 
     let mut region = Region::new(&input);
+    let best_scanner = region
+        .scanners
+        .iter_mut()
+        .max_by(|a, b| a.detectable_positions().cmp(&b.detectable_positions()))
+        .unwrap();
     println!(
         "Part 1: {} asteroids can be detected from the best station",
-        region.calculate_maximum_detectable_relations()
+        best_scanner.detectable_positions()
     );
+
+    // Part 2
+
+    best_scanner.vaporize_all_asteroids();
+    if let Some(vaporization) = best_scanner.vaporizations.get(199) {
+        let product = vaporization.x * 100 + vaporization.y;
+        println!("Part 2: the product for the 200th asteroid is {}", product);
+    }
 }
