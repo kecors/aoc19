cd8c2d2b972da9100a305be378d17199fb59318c
Kenneth Collins <dev@kecors.net>
2022-03-03 18:32:02 -0500

    Solve day 11, part 2

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -34,8 +34,7 @@ fn main() {
     let (tx_computer, rx_computer) = channel();
     let (tx_master, rx_master) = channel();
 
-    let debug_flag = false;
-    let mut computer = Computer::new(&program, rx_computer, tx_master, debug_flag);
+    let mut computer = Computer::new(&program, rx_computer, tx_master, false);
     thread::spawn(move || computer.run());
 
     let mut whites = HashSet::new();
@@ -88,7 +87,80 @@ fn main() {
         painted_positions.len()
     );
 
-    if debug_flag {
+    if false {
+        let min_x = whites.iter().map(|position| position.x).min().unwrap();
+        let max_x = whites.iter().map(|position| position.x).max().unwrap();
+        let min_y = whites.iter().map(|position| position.y).min().unwrap();
+        let max_y = whites.iter().map(|position| position.y).max().unwrap();
+        for y in min_y..=max_y {
+            for x in min_x..=max_x {
+                print!(
+                    "{}",
+                    if whites.contains(&Position { x, y }) {
+                        '#'
+                    } else {
+                        '.'
+                    }
+                );
+            }
+            println!();
+        }
+    }
+
+    // Part 2
+
+    let (tx_computer, rx_computer) = channel();
+    let (tx_master, rx_master) = channel();
+
+    let mut computer = Computer::new(&program, rx_computer, tx_master, false);
+    thread::spawn(move || computer.run());
+
+    let mut whites = HashSet::new();
+    let mut position = Position { x: 0, y: 0 };
+    let mut orientation = Orientation::North;
+
+    whites.insert(position);
+
+    loop {
+        let mut color = if whites.contains(&position) { 1 } else { 0 };
+        if tx_computer.send(color).is_err() {
+            break;
+        }
+
+        color = match rx_master.recv() {
+            Ok(new_color) => new_color,
+            Err(_) => break,
+        };
+        match color {
+            0 => whites.remove(&position),
+            1 => whites.insert(position),
+            _ => panic!("Received unexpected color {}", color),
+        };
+
+        let turn = match rx_master.recv() {
+            Ok(turn) => turn,
+            Err(_) => break,
+        };
+        orientation = match (turn, &orientation) {
+            (0, Orientation::North) => Orientation::West,
+            (1, Orientation::North) => Orientation::East,
+            (0, Orientation::East) => Orientation::North,
+            (1, Orientation::East) => Orientation::South,
+            (0, Orientation::South) => Orientation::East,
+            (1, Orientation::South) => Orientation::West,
+            (0, Orientation::West) => Orientation::South,
+            (1, Orientation::West) => Orientation::North,
+            _ => panic!("Unexpected turn {} and orientation {:?}", turn, orientation),
+        };
+        match orientation {
+            Orientation::North => position.y -= 1,
+            Orientation::East => position.x += 1,
+            Orientation::South => position.y += 1,
+            Orientation::West => position.x -= 1,
+        }
+    }
+
+    if true {
         let min_x = whites.iter().map(|position| position.x).min().unwrap();
         let max_x = whites.iter().map(|position| position.x).max().unwrap();
         let min_y = whites.iter().map(|position| position.y).min().unwrap();
