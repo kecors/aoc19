821d416bee56297d9683b4055958c795cb25dd28
Kenneth Collins <dev@kecors.net>
2022-03-07 14:39:27 -0500

    Solve day 12, part 2

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,8 +1,5 @@
-extern crate pest;
-#[macro_use]
-extern crate pest_derive;
-
 use pest::Parser;
+use pest_derive::Parser;
 use std::cmp::Ordering;
 use std::fmt;
 use std::io::{stdin, Read};
@@ -11,79 +8,77 @@ use std::io::{stdin, Read};
 #[grammar = "space.pest"]
 struct SpaceParser;
 
-#[derive(Debug, Clone, Copy)]
-struct Delta {
-    x: isize,
-    y: isize,
-    z: isize,
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Axis {
+    positions: Vec<isize>,
+    velocities: Vec<isize>,
 }
 
-impl Delta {
-    fn new() -> Delta {
-        Delta { x: 0, y: 0, z: 0 }
-    }
-}
+impl Axis {
+    fn new(positions: Vec<isize>) -> Axis {
+        let velocities = vec![0; positions.len()];
 
-#[derive(Debug)]
-struct Moon {
-    x: isize,
-    y: isize,
-    z: isize,
-    velocity_x: isize,
-    velocity_y: isize,
-    velocity_z: isize,
-}
-
-impl fmt::Display for Moon {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "pos=<x={:>2}, y={:>2}, z={:>2}>, vel=<x={:>2}, y={:>2}, z={:>2}>",
-            self.x, self.y, self.z, self.velocity_x, self.velocity_y, self.velocity_z
-        )
-    }
-}
-
-impl Moon {
-    fn new(x: isize, y: isize, z: isize) -> Moon {
-        let velocity_x = 0;
-        let velocity_y = 0;
-        let velocity_z = 0;
-
-        Moon {
-            x,
-            y,
-            z,
-            velocity_x,
-            velocity_y,
-            velocity_z,
+        Axis {
+            positions,
+            velocities,
         }
     }
 
-    fn potential_energy(&self) -> u32 {
-        (self.x.abs() + self.y.abs() + self.z.abs()) as u32
+    fn apply_gravity(&mut self) {
+        for (index_a, position_a) in self.positions.iter().enumerate() {
+            for position_b in self.positions.iter() {
+                self.velocities[index_a] += match position_a.cmp(position_b) {
+                    Ordering::Less => 1,
+                    Ordering::Equal => 0,
+                    Ordering::Greater => -1,
+                };
+            }
+        }
     }
 
-    fn kinetic_energy(&self) -> u32 {
-        (self.velocity_x.abs() + self.velocity_y.abs() + self.velocity_z.abs()) as u32
+    fn apply_velocity(&mut self) {
+        for (index, position) in self.positions.iter_mut().enumerate() {
+            *position += self.velocities[index];
+        }
     }
 
-    fn total_energy(&self) -> u32 {
-        self.potential_energy() * self.kinetic_energy()
+    fn count_to_reset(&mut self) -> u64 {
+        let baseline = self.clone();
+        let mut count = 0;
+        loop {
+            count += 1;
+            self.apply_gravity();
+            self.apply_velocity();
+            if *self == baseline {
+                break;
+            }
+        }
+
+        count
     }
 }
 
 #[derive(Debug)]
 struct Space {
-    moons: Vec<Moon>,
+    x_axis: Axis,
+    y_axis: Axis,
+    z_axis: Axis,
 }
 
 impl fmt::Display for Space {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut result = String::new();
 
-        for moon in self.moons.iter() {
-            result.push_str(&format!("{}\n", moon));
+        for index in 0..self.x_axis.positions.len() {
+            result.push_str(&format!(
+                "pos=<x={:>3}, y={:>3}, z={:>3}>, vel=<{:>3}, y={:>3}, z={:>3}>\n",
+                self.x_axis.positions[index],
+                self.y_axis.positions[index],
+                self.z_axis.positions[index],
+                self.x_axis.velocities[index],
+                self.y_axis.velocities[index],
+                self.z_axis.velocities[index]
+            ));
         }
 
         write!(f, "{}", result)
@@ -94,10 +89,9 @@ impl Space {
     fn new(input: &str) -> Space {
         let pairs = SpaceParser::parse(Rule::main, input).unwrap_or_else(|e| panic!("{}", e));
 
-        let mut moons = Vec::new();
-        let mut x: isize = 0;
-        let mut y: isize = 0;
-        let mut z: isize;
+        let mut x_positions = Vec::new();
+        let mut y_positions = Vec::new();
+        let mut z_positions = Vec::new();
 
         for pair in pairs {
             let rule = pair.as_rule();
@@ -105,15 +99,13 @@ impl Space {
 
             match rule {
                 Rule::x => {
-                    x = text.parse::<isize>().unwrap();
+                    x_positions.push(text.parse::<isize>().unwrap());
                 }
                 Rule::y => {
-                    y = text.parse::<isize>().unwrap();
+                    y_positions.push(text.parse::<isize>().unwrap());
                 }
                 Rule::z => {
-                    z = text.parse::<isize>().unwrap();
-                    let moon = Moon::new(x, y, z);
-                    moons.push(moon);
+                    z_positions.push(text.parse::<isize>().unwrap());
                 }
                 _ => {
                     panic!("Unknown rule {:?} with {:?}", rule, text);
@@ -121,77 +113,74 @@ impl Space {
             }
         }
 
-        Space { moons }
+        Space {
+            x_axis: Axis::new(x_positions),
+            y_axis: Axis::new(y_positions),
+            z_axis: Axis::new(z_positions),
+        }
     }
 
     fn step(&mut self) {
-        // Apply gravity
-        let mut deltas = vec![Delta::new(); self.moons.len()];
-
-        for index_a in 0..self.moons.len() {
-            for index_b in (index_a + 1)..self.moons.len() {
-                match self.moons[index_a].x.cmp(&self.moons[index_b].x) {
-                    Ordering::Less => {
-                        deltas[index_a].x += 1;
-                        deltas[index_b].x -= 1;
-                    }
-                    Ordering::Equal => {}
-                    Ordering::Greater => {
-                        deltas[index_a].x -= 1;
-                        deltas[index_b].x += 1;
-                    }
-                }
-                match self.moons[index_a].y.cmp(&self.moons[index_b].y) {
-                    Ordering::Less => {
-                        deltas[index_a].y += 1;
-                        deltas[index_b].y -= 1;
-                    }
-                    Ordering::Equal => {}
-                    Ordering::Greater => {
-                        deltas[index_a].y -= 1;
-                        deltas[index_b].y += 1;
-                    }
-                }
-                match self.moons[index_a].z.cmp(&self.moons[index_b].z) {
-                    Ordering::Less => {
-                        deltas[index_a].z += 1;
-                        deltas[index_b].z -= 1;
-                    }
-                    Ordering::Equal => {}
-                    Ordering::Greater => {
-                        deltas[index_a].z -= 1;
-                        deltas[index_b].z += 1;
-                    }
-                }
-            }
-        }
-        for index in 0..self.moons.len() {
-            self.moons[index].velocity_x += deltas[index].x;
-            self.moons[index].velocity_y += deltas[index].y;
-            self.moons[index].velocity_z += deltas[index].z;
-        }
-
-        // Apply velocity
-        for index in 0..self.moons.len() {
-            self.moons[index].x += self.moons[index].velocity_x;
-            self.moons[index].y += self.moons[index].velocity_y;
-            self.moons[index].z += self.moons[index].velocity_z;
-        }
+        self.x_axis.apply_gravity();
+        self.x_axis.apply_velocity();
+        self.y_axis.apply_gravity();
+        self.y_axis.apply_velocity();
+        self.z_axis.apply_gravity();
+        self.z_axis.apply_velocity();
     }
 
-    fn total_energy(&self) -> u32 {
-        self.moons.iter().map(|moon| moon.total_energy()).sum()
+    fn total_energy(&self) -> isize {
+        let mut sum = 0;
+
+        for index in 0..self.x_axis.positions.len() {
+            let potential = self.x_axis.positions[index].abs()
+                + self.y_axis.positions[index].abs()
+                + self.z_axis.positions[index].abs();
+            let kinetic = self.x_axis.velocities[index].abs()
+                + self.y_axis.velocities[index].abs()
+                + self.z_axis.velocities[index].abs();
+            let product = potential * kinetic;
+            sum += product;
+        }
+
+        sum
     }
+}
+
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    while a > 0 {
+        let temp = a;
+        a = b % a;
+        b = temp;
+    }
+
+    b
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    a * b / gcd(a, b)
 }
 
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
 
+    // Part 1
+
     let mut space = Space::new(&input);
+    //println!("After 0 steps:\n{}", space);
     for _step in 1..=1000 {
         space.step();
         //println!("After {} steps:\n{}", step, space);
     }
     println!("Part 1: the total energy is {}", space.total_energy());
+
+    // Part 2
+
+    let mut space = Space::new(&input);
+    let x_count = space.x_axis.count_to_reset();
+    let y_count = space.y_axis.count_to_reset();
+    let z_count = space.z_axis.count_to_reset();
+    let steps = lcm(x_count, lcm(y_count, z_count));
+    println!("Part 2: it takes {} steps", steps);
 }
